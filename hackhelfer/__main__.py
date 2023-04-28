3e140f6a3bc0a0b2cbdcb796a4759a752586b5e1
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-04-28 13:56:42 +0200

    add argparse

--- hackhelfer/__main__.py
+++ hackhelfer/__main__.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python3
 
 import sys
+import argparse
 from .hackhelfer import HackHelfer
 
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+            prog='hackhelfer',
+            description='Attack-Simulator for Testbeds')
+    parser.add_argument(
+            '--config',
+            help='Attack-Playbook in yaml-format',
+            required=True)
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    hacky = HackHelfer("example.yml")
+    args = parse_args()
+    hacky = HackHelfer(args.config)
     sys.exit(hacky.main())
