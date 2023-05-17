b0580bc6343b108afac0e6a7c41047bd2b7443e7
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-05-17 13:47:08 +0200

    changed hackhelfer.log to penpal.log

--- penpal/__main__.py
+++ penpal/__main__.py
@@ -33,7 +33,7 @@ def initialize_logger():
     formatter = ColoredFormatter(LOGFORMAT, datefmt="%Y-%m-%d %H:%M:%S")
     console_handler.setFormatter(formatter)
     playbook_logger.addHandler(console_handler)
-    file_handler = logging.FileHandler("hackhelfer.log", mode='w')
+    file_handler = logging.FileHandler("penpal.log", mode='w')
     formatter = logging.Formatter(
             '%(asctime)s %(levelname)s - %(message)s',
             datefmt="%Y-%m-%d %H:%M:%S")
