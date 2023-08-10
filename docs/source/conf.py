168faf3e061a3a973d80b5dae5ed94679426426e
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-08-10 15:18:49 +0200

    set version to 0.1.0

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 project = 'PenPal'
 copyright = '2023, Wolfgang Hotwagner'
 author = 'Wolfgang Hotwagner'
-release = '1.0.0'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- penpal/metadata.py
+++ penpal/metadata.py
@@ -1,14 +1,14 @@
 __authors__ = ["Wolfgang Hotwagner"]
 __contact__ = "aecid@ait.ac.at"
 __copyright__ = "Copyright 2023, AIT Austrian Institute of Technology GmbH"
-__date__ = "2023/04/28"
+__date__ = "2023/08/10"
 __deprecated__ = False
 __email__ = "aecid@ait.ac.at"
 __website__ = "https://aecid.ait.ac.at"
 __license__ = "GPLv3"
-__maintainer__ = "Wolfgang Hotwagner"
+__maintainer__ = "Wolfgang Hotwagner, Max Landauer, Markus Wurzenberger, Florian Skopik"
 __status__ = "Production"
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 __version_string__ = (
         f"(Austrian Institute of Technology)\t"
         f"{__website__}\tVersion: {__version__}")
