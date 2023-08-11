8641667b05ec55a5e5d9fb26c22b58c423d92ad0
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-08-11 12:51:13 +0200

    Introduced variablestore
    The global variablestore is a new way of storing and using variables.
    It has a clear interface and will replace the varparse.

---
+++ penpal/variablestore.py
@@ -0,0 +1,34 @@
+from string import Template
+from typing import Any
+
+
+class VariableStore:
+    def __init__(self):
+        self.variables = {}
+
+    def from_dict(self, variables: dict):
+        for k, v in variables.items():
+            name = self.remove_sign(k)
+            self.variables[name] = v
+
+    def remove_sign(self, name: str, sign: str = '$') -> str:
+        if name.startswith(sign):
+            return name[1:]
+        else:
+            return name
+
+    def substitute_str(self, template_str: str) -> str:
+        temp = Template(template_str)
+        return temp.safe_substitute(self.variables)
+
+    def set_variable(self, variable: str, value: str):
+        self.variables[self.remove_sign(variable)] = value
+
+    def get_variable(self, variable: str):
+        return self.variables[variable]
+
+    def substitute(self, data: Any) -> Any:
+        if isinstance(data, str):
+            return self.substitute_str(data)
+        else:
+            return data
