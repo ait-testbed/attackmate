364c0772cd5d8f1b0cd4699bc200b37f74fb1953
hoti <code@feedyourhead.at>
2023-09-13 22:09:47 +0200

    added IncludeCommand to background-blacklist

--- src/attackmate/schemas.py
+++ src/attackmate/schemas.py
@@ -29,7 +29,7 @@ class BaseCommand(BaseModel):
 
     @validator('background')
     def bg_not_implemented_yet(cls, v):
-        if cls in (SSHCommand, SFTPCommand, MsfModuleCommand):
+        if cls in (SSHCommand, SFTPCommand, MsfModuleCommand, IncludeCommand):
             raise ValueError("background mode is unsupported for this command")
         return v
 
