9e59dc7e40b328e8913fdbf50acd980f2d2f088d
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-09-29 23:43:10 +0200

    moved logger-output of msf-cleanup

--- src/attackmate/msfsessionexecutor.py
+++ src/attackmate/msfsessionexecutor.py
@@ -1,10 +1,10 @@
 import atexit
 
+from pymetasploit3.msfrpc import MsfRpcClient, MsfAuthError
 from attackmate.variablestore import VariableStore
 from .baseexecutor import BaseExecutor
 from .execexception import ExecException
 from .result import Result
-from pymetasploit3.msfrpc import MsfRpcClient, MsfAuthError
 from .schemas import MsfSessionCommand, BaseCommand
 from .msfsessionstore import MsfSessionStore
 from .processmanager import ProcessManager
@@ -22,8 +22,8 @@ class MsfSessionExecutor(BaseExecutor):
         super().__init__(pm, varstore, cmdconfig)
 
     def cleanup(self):
-        self.logger.debug("Killing all Meterpreter-Sessions")
         if self.msf is not None:
+            self.logger.debug('Killing all Meterpreter-Sessions')
             for session_id in self.msf.sessions.list.keys():
                 self.msf.sessions.session(session_id).stop()
 
@@ -39,40 +39,40 @@ class MsfSessionExecutor(BaseExecutor):
 
     def log_command(self, command: BaseCommand):
         if self.msf is None:
-            self.logger.debug("Connecting to msf-server...")
+            self.logger.debug('Connecting to msf-server...')
             self.connect(self.msfconfig)
         self.logger.info(f"Executing Msf-Session-Command: '{command.cmd}'")
 
     def _exec_cmd(self, command: MsfSessionCommand) -> Result:
         if self.msf is None:
-            raise ExecException("ConnectionError")
+            raise ExecException('ConnectionError')
 
         session_id = self.sessionstore.get_session_by_name(command.session, self.msf.sessions)
-        self.logger.debug(f"Using session-id: {session_id}")
+        self.logger.debug(f'Using session-id: {session_id}')
         return_empty = False
 
         try:
 
             if command.stdapi:
-                self.logger.info("Loading stapi")
+                self.logger.info('Loading stapi')
                 self.msf.sessions.session(session_id).write('load stdapi')
             if command.write:
-                self.logger.info("Writing raw-data in msf-session")
+                self.logger.info('Writing raw-data in msf-session')
                 self.msf.sessions.session(session_id).write(command.cmd)
-                output = ""
+                output = ''
                 return_empty = True
             if command.read:
-                self.logger.info("Reading raw-data in msf-session")
+                self.logger.info('Reading raw-data in msf-session')
                 output = self.msf.sessions.session(session_id).read()
                 return Result(output, 0)
 
             if not return_empty:
-                self.logger.info("Executing a msf-command")
+                self.logger.info('Executing a msf-command')
                 try:
                     output = self.msf.sessions.session(session_id).run_with_output(command.cmd,
                                                                                    command.end_str)
                 except TypeError:
-                    self.logger.debug("Fallback: First raw-write again and then raw-read data in msf-session")
+                    self.logger.debug('Fallback: First raw-write again and then raw-read data in msf-session')
                     self.msf.sessions.session(session_id).write(command.cmd)
                     output = self.msf.sessions.session(session_id).read()
 
