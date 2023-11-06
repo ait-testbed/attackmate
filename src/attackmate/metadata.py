cffc8bcd764af4bf067adcad9c289add86765c05
Wolfgang Hotwagner <wolfgang.hotwagner@ait.ac.at>
2023-11-06 10:06:42 +0100

    updated version to 0.2.1

--- src/attackmate/metadata.py
+++ src/attackmate/metadata.py
@@ -1,17 +1,17 @@
-__authors__ = ["Wolfgang Hotwagner"]
-__contact__ = "aecid@ait.ac.at"
-__copyright__ = "Copyright 2023, AIT Austrian Institute of Technology GmbH"
-__date__ = "2023/08/10"
+__authors__ = ['Wolfgang Hotwagner']
+__contact__ = 'aecid@ait.ac.at'
+__copyright__ = 'Copyright 2023, AIT Austrian Institute of Technology GmbH'
+__date__ = '2023/08/10'
 __deprecated__ = False
-__email__ = "aecid@ait.ac.at"
-__website__ = "https://aecid.ait.ac.at"
-__license__ = "GPLv3"
-__maintainer__ = "Wolfgang Hotwagner, Max Landauer, Markus Wurzenberger, Florian Skopik"
-__status__ = "Production"
-__version__ = "0.2.0"
+__email__ = 'aecid@ait.ac.at'
+__website__ = 'https://aecid.ait.ac.at'
+__license__ = 'GPLv3'
+__maintainer__ = 'Wolfgang Hotwagner, Max Landauer, Markus Wurzenberger, Florian Skopik'
+__status__ = 'Production'
+__version__ = '0.2.1'
 __version_string__ = (
-        f"(Austrian Institute of Technology)\t"
-        f"{__website__}\tVersion: {__version__}")
+        f'(Austrian Institute of Technology)\t'
+        f'{__website__}\tVersion: {__version__}')
 __all__ = [
         '__authors__',
         '__contact__',
