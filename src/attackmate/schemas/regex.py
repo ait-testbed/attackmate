6b51c85e3ac21b953a01abf570172f3e0ebeded8
kali <kali@kali>
2024-06-24 15:56:39 +0200

    log informative error if rmode is sub and replace is missing"

--- src/attackmate/schemas/regex.py
+++ src/attackmate/schemas/regex.py
@@ -2,21 +2,27 @@ from typing import Literal, Optional
 from pydantic import ValidationInfo, field_validator
 from .base import BaseCommand
 from pprint import pprint
+import logging
 
+logger = logging.getLogger('playbook')
 
 class RegExCommand(BaseCommand):
+    
     @field_validator('mode')
     @classmethod
     def sub_needs_replace(cls, v, info: ValidationInfo) -> str:
-        if 'replace' in info.data and info.data['replace']:
-            return v
-        else:
+
+        if v == 'sub' and not info.data.get('replace'):
             print(info.data)
             pprint(info)
-            raise ValueError('Regex: sub needs the replace-setting!')
+            logger.error('Error parsing playbook. regex command mode: sub must be preceded by the replace setting!')
+            raise ValueError('Regex sub mode needs replace-setting!')
+        
+        return v
+            
 
     type: Literal['regex']
-    # replace is a dependency for mode, therefor it must be place BEFORE mode!
+    # replace is a dependency for mode, therefore it must be placed BEFORE mode!
     replace: Optional[str] = None
     mode: Literal['search', 'split', 'findall', 'sub'] = 'findall'
     input: str = 'RESULT_STDOUT'
