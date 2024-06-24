0b450cdeb4216335c7798a1fade4b7625eb66516
kali <kali@kali>
2024-06-24 15:51:16 +0200

    rerefactor: use enumerate in forge_variable, first match always MATCH_0"

--- src/attackmate/executors/common/regexexecutor.py
+++ src/attackmate/executors/common/regexexecutor.py
@@ -14,32 +14,31 @@ import re
 
 
 class RegExExecutor(BaseExecutor):
+    
+    def log_command(self, command: RegExCommand):
+        self.logger.warn(f"RegEx: '{command.cmd}', Mode: '{command.mode}'")
 
+    
     def forge_variables(self, data, variable_name='MATCH'):
         result = {}
         if data is None:
             return None
         if isinstance(data, str):
-            result[variable_name] = data
-        elif isinstance(data, list) or isinstance(data, tuple):
-            i = 0
-            for item in data:
+            result[f'{variable_name}_0'] = data
+        elif isinstance(data, (list, tuple)):
+            for i, item in enumerate(data):
                 tmpvar = f'{variable_name}_{str(i)}'
                 if isinstance(item, str):
                     result[tmpvar] = item
-                elif isinstance(item, list) or isinstance(item, tuple):
-                    j = 0
-                    for level2 in item:
+                elif isinstance(item, (list, tuple)):
+                    for j, level2 in enumerate(item):
                         tmpvar2 = f'{tmpvar}_{str(j)}'
                         if isinstance(level2, str):
                             result[tmpvar2] = level2
-                        j += 1
-                i += 1
+
         return result
 
-    def log_command(self, command: RegExCommand):
-        self.logger.warn(f"RegEx: '{command.cmd}'")
-
+    
     def register_outputvars(self, outputvars: dict, matches):
         if not matches:
             self.logger.debug('no match!')
@@ -49,28 +48,28 @@ class RegExExecutor(BaseExecutor):
             temp = Template(v)
             self.varstore.set_variable(k, temp.safe_substitute(matches))
 
+    def forge_and_register_variables(self, output: dict, data) -> dict:
+        matches = self.forge_variables(data)
+        self.logger.debug(matches)
+        self.register_outputvars(output, matches)
+        return
+
     def _exec_cmd(self, command: RegExCommand) -> Result:
         self.setoutuptvars = False
         if command.mode == 'findall':
             m = re.findall(command.cmd, self.varstore.get_variable(command.input))
-            matches = self.forge_variables(m)
-            self.logger.debug(matches)
-            self.register_outputvars(command.output, matches)
+            self.forge_and_register_variables(command.output, m)
         if command.mode == 'split':
             m = re.split(command.cmd, self.varstore.get_variable(command.input))
-            matches = self.forge_variables(m)
-            self.logger.debug(matches)
-            self.register_outputvars(command.output, matches)
+            self.forge_and_register_variables(command.output, m)
         if command.mode == 'search':
+            # search() module will only return the first occurrence that matches the specified pattern.
             m3 = re.search(command.cmd, self.varstore.get_variable(command.input))
             if m3 is not None and isinstance(m3, Match):
-                matches = self.forge_variables(m3.groups())
-                self.logger.debug(matches)
-                self.register_outputvars(command.output, matches)
+                self.forge_and_register_variables(command.output, m3.group())
         if command.mode == 'sub':
             if command.replace:
                 replaced = re.sub(command.cmd, command.replace, self.varstore.get_variable(command.input))
-                matches = self.forge_variables(replaced)
-                self.register_outputvars(command.output, matches)
+                self.forge_and_register_variables(command.output, replaced)
 
         return Result('', 0)
