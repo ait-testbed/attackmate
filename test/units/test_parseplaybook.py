05cf1f7b6a11083f90fce63c7ad4bcf7e26db1b3
thorinaboenke <thorina-boenke@web.de>
2024-07-25 08:03:40 +0200

    remove comments

--- test/units/test_parseplaybook.py
+++ test/units/test_parseplaybook.py
@@ -5,8 +5,6 @@ from pathlib import Path
 import yaml
 from attackmate.__main__ import parse_playbook
 
-# import other frunciotn sfrom other modules to mock: yaml _load?
-
 
 # Mock Playbook class for validation
 class MockPlaybook:
@@ -43,18 +41,18 @@ def test_parse_playbook_valid_file(mock_logger, mock_playbook_file):
         'attackmate.__main__.yaml.safe_load', return_value=yaml.safe_load(sample_playbook_yaml)
     ), patch('attackmate.__main__.Playbook', MockPlaybook):
 
-        # Call the function with the path to the temporary playbook file
         result = parse_playbook(str(mock_playbook_file), mock_logger)
 
-        # Assertions
         assert result == 'MockPlaybookObject'
 
 
 def test_parse_playbook_nonexistent_file(mock_logger):
     non_existent_file = Path('/non/existent/path/playbook.yml')
+    error_message = (
+        f"Error: Playbook file not found under '{non_existent_file}' "
+        "or in the current directory or in /etc/attackmate/playbooks"
+    )
 
     with pytest.raises(SystemExit):
         parse_playbook(str(non_existent_file), mock_logger)
-    mock_logger.error.assert_called_with(
-        f"Error: Playbook file not found under '{non_existent_file}' or in the current directory or in /etc/attackmate/playbooks"
-    )
+    mock_logger.error.assert_called_with(error_message)
