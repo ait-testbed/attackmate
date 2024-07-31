a7d7f6c6331bbdea388b0c04891c868751b32415
thorinaboenke <thorina-boenke@web.de>
2024-07-31 14:18:15 +0200

    add unit test for regex support in conditionals

--- test/units/test_conditionals.py
+++ test/units/test_conditionals.py
@@ -54,7 +54,6 @@ class TestConditionals:
         assert Conditional.test('1 is True')
         assert Conditional.test('0 is True') is False
 
-
     def test_exceptions(self):
         with pytest.raises(ConditionalError, match='Unknown Condition:'):
             Conditional.test('{"a":1, **d}')
@@ -66,3 +65,36 @@ class TestConditionals:
             Conditional.test('not (a + b)')
         with pytest.raises(ConditionalError, match='Unknown compare operation'):
             Conditional.test('a in b')
+
+    def test_regex(self):
+        # exact matches
+        assert Conditional.test('foo =~ foo') is True
+        assert Conditional.test('foo =~ bar') is False
+        assert Conditional.test('foo !~ foo') is False
+        assert Conditional.test('foo !~ bar') is True
+
+        assert Conditional.test('foo =~ foo') is True
+        assert Conditional.test('foo =~ bar') is False
+        assert Conditional.test('foo !~ foo') is False
+        assert Conditional.test('foo !~ bar') is True
+
+        # matches somewhere in the string
+        assert Conditional.test('somethingfoo =~ foo') is True
+        assert Conditional.test('foosomething =~ bar') is False
+        assert Conditional.test('somehtingfoo !~ foo') is False
+        assert Conditional.test('foosomething !~ bar') is True
+
+        # matches with wildcards and ranges
+        assert Conditional.test('hello =~ h.llo') is True  # Single character wildcard
+        assert Conditional.test('hello =~ h.*o') is True  # Zero or more characters
+        assert Conditional.test('hello =~ h[aeiou]llo') is True  # Character set
+        assert Conditional.test('hello =~ h[^aeiou]llo') is False  # Negated character set
+        assert Conditional.test('hello123 =~ h.*\\d+') is True  # Digits
+        assert Conditional.test('hello =~ ^h.*o$') is True  # Start and end of string
+
+        assert Conditional.test('hello !~ h.llo') is False
+        assert Conditional.test('hello !~ h.*o') is False
+        assert Conditional.test('hello !~ h[aeiou]llo') is False
+        assert Conditional.test('hello !~ h[^aeiou]llo') is True
+        assert Conditional.test('hello123 !~ h.*\\d+') is False
+        assert Conditional.test('hello !~ ^h.*o$') is False
