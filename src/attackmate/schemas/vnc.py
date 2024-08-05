da905d84b4e36fa310d1e7a2bd7bc606412d7c62
thorinaboenke <thorina-boenke@web.de>
2024-08-05 16:31:38 +0200

    add command parameters and model validation

--- src/attackmate/schemas/vnc.py
+++ src/attackmate/schemas/vnc.py
@@ -1,8 +1,33 @@
 from typing import Optional, Literal
 from .base import BaseCommand, StringNumber
+from pydantic import model_validator
 
 
 class VncCommand(BaseCommand):
     type: Literal['vnc']
+    cmd: Literal['key', 'type', 'move', 'capture', 'click']
     hostname: Optional[str] = None
-    port: Optional[StringNumber] = 5900
+    port: Optional[StringNumber] = None
+    display: Optional[StringNumber] = None
+    password: Optional[str] = None
+    key: Optional[str] = None
+    input: Optional[str] = None
+    filename: Optional[str] = None
+    x: Optional[int] = None
+    y: Optional[int] = None
+
+    @model_validator(mode='after')
+    def check_cmd_requirements(cls, values):
+        cmd = values.cmd
+
+        if cmd == 'type' and values.input is None:
+            raise ValueError('Command "type" requires an "input" field.')
+        elif cmd == 'key' and values.key is None:
+            raise ValueError('Command "key" requires a "key" field.')
+        elif cmd == 'capture' and values.filename is None:
+            raise ValueError('Command "capture" requires a "filename" field.')
+        elif cmd == 'move':
+            if values.x is None or values.y is None:
+                raise ValueError('Command "move" requires both "x" and "y" fields.')
+
+        return values
