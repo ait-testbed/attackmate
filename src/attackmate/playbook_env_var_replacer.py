ddf91ad5c303a91709e1e818ac9c06fa19a7e061
thorinaboenke <thorina-boenke@web.de>
2024-09-10 12:04:45 +0200

    add logic to  replace prefixed playbook vars with env vars

--- src/attackmate/playbook_env_var_replacer.py
+++ src/attackmate/playbook_env_var_replacer.py
@@ -5,8 +5,10 @@ import re
 def replace_env_variables_in_playbook(content: str) -> str:
     """Replaces environment variables in the content with their actual values.
     First parses the variable names in the playbook vars section (between
-    "vars:" and "commands:"), and saves them in a set. The variables from the vars
-    section will not be replaced by environmental variables anywhere in the playbook.
+    "vars:" and "commands:"), and saves them in a set. Variablenames defined
+    in the vars section will only be replaced by environmental variables values
+    if they are prefixed with ATTACKMATE_, and only if the env variable (
+    without the attackmate prefix) exists
 
     ## Sample playbook ##
     vars:
@@ -43,15 +45,33 @@ def replace_env_variables_in_playbook(content: str) -> str:
         vars_section = vars_section_match.group(1)
         # find all variables in the vars section
         vars_section_var_names = set(vars_section_var_pattern.findall(vars_section))
+        # filter for variables in the vars section prefixed with ATTACKMATE_
+        vars_section_var_names_with_attackmate_prefix = {
+            var for var in vars_section_var_names if var.startswith('ATTACKMATE_')
+        }
     else:
         vars_section_var_names = set()
+        vars_section_var_names_with_attackmate_prefix = set()
 
     def replace_variable(match):
         variable_name = match.group(1)
 
-        # Do not replace if it is a variable name from the vars section
-        if variable_name in vars_section_var_names:
+        # Do not replace if it is a variable name from the vars section not prefixed with ATTACKMATE_
+        if (
+            variable_name in vars_section_var_names
+            and variable_name not in vars_section_var_names_with_attackmate_prefix
+        ):
             return f'${variable_name}'
+
+        if variable_name in vars_section_var_names_with_attackmate_prefix:
+            # Remove the ATTACKMATE_ prefix from the variable name
+            env_var_name = variable_name[len('ATTACKMATE_') :]
+
+            # Check if the environment variable without the prefix exists
+            value = os.getenv(env_var_name)
+
+            # If it exists, return its value, otherwise return the variable name with the prefix
+            return value if value is not None else f'${variable_name}'
 
         # Otherwise replace with env variable value
         value = os.getenv(variable_name)
