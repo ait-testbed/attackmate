24f44910f137a55a31012e4051baa208010699ef
thorinaboenke <thorina-boenke@web.de>
2024-11-08 15:00:53 +0100

    add vnc command to loop

--- src/attackmate/schemas/loop.py
+++ src/attackmate/schemas/loop.py
@@ -4,25 +4,26 @@ from typing import Literal
 from typing import List, Union
 from .sleep import SleepCommand
 from .shell import ShellCommand
+from .vnc import VncCommand
 from .setvar import SetVarCommand
 from .include import IncludeCommand
-from .metasploit import (MsfModuleCommand,
-                         MsfSessionCommand,
-                         MsfPayloadCommand)
+from .metasploit import MsfModuleCommand, MsfSessionCommand, MsfPayloadCommand
 
-from .sliver import (SliverSessionCDCommand,
-                     SliverSessionLSCommand,
-                     SliverSessionNETSTATCommand,
-                     SliverSessionEXECCommand,
-                     SliverSessionMKDIRCommand,
-                     SliverSessionSimpleCommand,
-                     SliverSessionDOWNLOADCommand,
-                     SliverSessionUPLOADCommand,
-                     SliverSessionPROCDUMPCommand,
-                     SliverSessionRMCommand,
-                     SliverSessionTERMINATECommand,
-                     SliverHttpsListenerCommand,
-                     SliverGenerateCommand)
+from .sliver import (
+    SliverSessionCDCommand,
+    SliverSessionLSCommand,
+    SliverSessionNETSTATCommand,
+    SliverSessionEXECCommand,
+    SliverSessionMKDIRCommand,
+    SliverSessionSimpleCommand,
+    SliverSessionDOWNLOADCommand,
+    SliverSessionUPLOADCommand,
+    SliverSessionPROCDUMPCommand,
+    SliverSessionRMCommand,
+    SliverSessionTERMINATECommand,
+    SliverHttpsListenerCommand,
+    SliverGenerateCommand,
+)
 from .ssh import SSHCommand, SFTPCommand
 from .http import WebServCommand, HttpClientCommand
 from .father import FatherCommand
@@ -31,36 +32,39 @@ from .debug import DebugCommand
 from .regex import RegExCommand
 
 
-Commands = List[Union[
-                         ShellCommand,
-                         MsfModuleCommand,
-                         MsfSessionCommand,
-                         MsfPayloadCommand,
-                         SleepCommand,
-                         SSHCommand,
-                         FatherCommand,
-                         SFTPCommand,
-                         DebugCommand,
-                         SetVarCommand,
-                         RegExCommand,
-                         TempfileCommand,
-                         IncludeCommand,
-                         WebServCommand,
-                         HttpClientCommand,
-                         SliverSessionCDCommand,
-                         SliverSessionLSCommand,
-                         SliverSessionNETSTATCommand,
-                         SliverSessionEXECCommand,
-                         SliverSessionMKDIRCommand,
-                         SliverSessionSimpleCommand,
-                         SliverSessionDOWNLOADCommand,
-                         SliverSessionUPLOADCommand,
-                         SliverSessionPROCDUMPCommand,
-                         SliverSessionRMCommand,
-                         SliverSessionTERMINATECommand,
-                         SliverHttpsListenerCommand,
-                         SliverGenerateCommand
-                         ]]
+Commands = List[
+    Union[
+        ShellCommand,
+        MsfModuleCommand,
+        MsfSessionCommand,
+        MsfPayloadCommand,
+        SleepCommand,
+        SSHCommand,
+        FatherCommand,
+        SFTPCommand,
+        DebugCommand,
+        SetVarCommand,
+        RegExCommand,
+        VncCommand,
+        TempfileCommand,
+        IncludeCommand,
+        WebServCommand,
+        HttpClientCommand,
+        SliverSessionCDCommand,
+        SliverSessionLSCommand,
+        SliverSessionNETSTATCommand,
+        SliverSessionEXECCommand,
+        SliverSessionMKDIRCommand,
+        SliverSessionSimpleCommand,
+        SliverSessionDOWNLOADCommand,
+        SliverSessionUPLOADCommand,
+        SliverSessionPROCDUMPCommand,
+        SliverSessionRMCommand,
+        SliverSessionTERMINATECommand,
+        SliverHttpsListenerCommand,
+        SliverGenerateCommand,
+    ]
+]
 
 
 class LoopCommand(BaseCommand):
