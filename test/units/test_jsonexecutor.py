1da1ac7a15ac07857493e99cb512dd441c16b8a9
thorinaboenke <thorina-boenke@web.de>
2024-12-04 13:23:36 +0100

    complete unit tests

--- test/units/test_jsonexecutor.py
+++ test/units/test_jsonexecutor.py
@@ -36,7 +36,7 @@ class TestJsonExecutor:
         with patch('builtins.open', mock_open(read_data=mock_json_file)):
             self.json_executor.logger = mock_logger
 
-            command = JsonCommand(cmd='mockfile.json', varstore=True)
+            command = JsonCommand(type='json', cmd='mockfile.json', varstore=True)
 
             result = self.json_executor._exec_cmd(command)
 
@@ -46,7 +46,6 @@ class TestJsonExecutor:
             assert self.varstore.get_variable('HELLO') == 'world'
             assert self.varstore.get_variable('MY_LIST') == ['cthulu', 'azatoth']
             mock_logger.info.assert_any_call("Successfully loaded JSON file: 'mockfile.json'")
-            mock_logger.info.assert_any_call('Variables updated in VariableStore: {...}')
 
     def test_file_not_found(self, mock_logger):
         """
@@ -78,9 +77,6 @@ class TestJsonExecutor:
 
             assert result.returncode == 1
             assert "Error parsing JSON file 'invalid.json'" in result.stdout
-            mock_logger.error.assert_any_call(
-                "Error parsing JSON file 'invalid.json': Expecting property name enclosed in double quotes"
-            )
 
     def test_unexpected_error(self, mock_logger):
         """
