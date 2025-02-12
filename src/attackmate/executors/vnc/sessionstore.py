e075e8a20d8c685db633939b45c2eab5c44ac0fb
kali <thorina-boenke@web.de>
2025-02-12 10:52:27 +0100

    remove get state from vnc session store

--- src/attackmate/executors/vnc/sessionstore.py
+++ src/attackmate/executors/vnc/sessionstore.py
@@ -5,6 +5,14 @@ class SessionStore:
     def __init__(self):
         self.store: dict[str, VncClient] = {}
 
+    def __getstate__(self):
+        """
+        store contains the clients of the vnc connections.
+        """
+        state = self.__dict__.copy()
+        state['store'] = None
+        return state
+
     def has_session(self, session_name: str) -> bool:
         if session_name in self.store:
             return True
