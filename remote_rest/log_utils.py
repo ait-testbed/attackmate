83fee892b038eb8e1f8ade11d6461d9b78da089f
kali <thorina-boenke@web.de>
2025-07-15 15:46:28 +0200

    add json to remote logging

--- remote_rest/log_utils.py
+++ remote_rest/log_utils.py
@@ -10,18 +10,19 @@ LOG_DIR = os.path.join(os.getcwd(), "attackmate_server_logs")
 # LOG_DIR = "/var/log/attackmate_instances" # must exists and has write permissions
 
 # List of logger names to add instance-specific handlers to
-TARGET_LOGGER_NAMES = ['playbook', 'output']  # json not needed
+TARGET_LOGGER_NAMES = ['playbook', 'output', 'json']
 
 # Create  formatter for the instance files
 instance_log_formatter = logging.Formatter(
     '%(asctime)s %(levelname)s - %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S'
 )
+json_log_formatter = logging.Formatter('%(message)s')
 
 
 @contextmanager
 def instance_logging(instance_id: str, log_level: int = logging.INFO):
-    """
+    """cd
     Context manager to temporarily add a file handler for a specific instance
     to the target AttackMate loggers.
     """
@@ -36,6 +37,7 @@ def instance_logging(instance_id: str, log_level: int = logging.INFO):
         timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
         instance_output_log_file = os.path.join(LOG_DIR, f"{timestamp}_{instance_id}_output.log")
         instance_attackmate_log_file = os.path.join(LOG_DIR, f"{timestamp}_{instance_id}_attackmate.log")
+        instance_json_log_file = os.path.join(LOG_DIR, f"{timestamp}_{instance_id}_attackmate.json")
 
         # instance-specific file handler
         #  'a' to append within the same request if multiple logs occur
@@ -48,21 +50,32 @@ def instance_logging(instance_id: str, log_level: int = logging.INFO):
         attackmate_file_handler.setFormatter(instance_log_formatter)
         attackmate_file_handler.setLevel(log_level)
 
+        attackmate_json_handler = logging.FileHandler(instance_json_log_file, mode='a')
+        attackmate_json_handler.setFormatter(json_log_formatter)
+        attackmate_json_handler.setLevel(log_level)
+
         # Add the handler to the target loggers
         for logger_name in TARGET_LOGGER_NAMES:
             logger = logging.getLogger(logger_name)
             logger.setLevel(log_level)
+            logger.propagate = False
             if logger_name == 'playbook':
                 logger.addHandler(attackmate_file_handler)
                 handlers.append(attackmate_file_handler)  # remove later finally
             if logger_name == 'output':
                 logger.addHandler(output_file_handler)
                 handlers.append(output_file_handler)  # remove later in finally
+            if logger_name == 'json':
+                logger.addHandler(attackmate_json_handler)
+                handlers.append(attackmate_json_handler)  # remove later in finally
             logging.info(
                 (f"Added instance log handlers for '{instance_id}' to logger '{logger_name}' -> "
-                 f"{instance_output_log_file} and {instance_attackmate_log_file}")
-            )
-        yield [instance_attackmate_log_file, instance_output_log_file]  # 'with' block executes here
+                 f"{instance_output_log_file} and {instance_attackmate_log_file} and {instance_json_log_file}."))
+        yield [
+            instance_attackmate_log_file,
+            instance_output_log_file,
+            instance_json_log_file
+        ]  # 'with' block executes here and uses these paths
 
     except Exception as e:
         logging.error(f"Error setting up instance logging for '{instance_id}': {e}", exc_info=True)
@@ -71,10 +84,11 @@ def instance_logging(instance_id: str, log_level: int = logging.INFO):
     finally:
         logging.info(f"Removing instance log handlers for '{instance_id}'...")
         for handler in handlers:
-            try:
-                for logger_name in TARGET_LOGGER_NAMES:
-                    logger = logging.getLogger(logger_name)
+            for logger_name in TARGET_LOGGER_NAMES:
+                logger = logging.getLogger(logger_name)
+                if handler in logger.handlers:
                     logger.removeHandler(handler)
+            try:
                 handler.close()
             except Exception as e:
                 logging.error(
