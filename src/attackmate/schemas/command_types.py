adeae7e71099ae86635b4949fb872ad28b182ccd
thorinaboenke <thorina-boenke@web.de>
2025-12-02 15:10:59 +0100

    make Command an annotated union

--- src/attackmate/schemas/command_types.py
+++ src/attackmate/schemas/command_types.py
@@ -1,14 +1,15 @@
 
 
-from typing import List, TypeAlias, Union
+from typing import Annotated, List, TypeAlias, Union
 from attackmate.schemas.command_subtypes import RemotelyExecutableCommand
 from attackmate.schemas.remote import AttackMateRemoteCommand
+from pydantic import Field
 
 
-Command: TypeAlias = Union[
-        RemotelyExecutableCommand,
-        AttackMateRemoteCommand
-    ]
+Command: TypeAlias = Annotated[Union[
+    RemotelyExecutableCommand,
+    AttackMateRemoteCommand
+], Field(discriminator='type')]
 
 
 Commands:  TypeAlias = List[Command]
