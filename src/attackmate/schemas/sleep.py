04c8fdf8c0f5d2df52522cf0df5c802183daf6fc
thorinaboenke <thorina-boenke@web.de>
2026-01-21 12:52:47 +0100

    calculate sleep time in computed field

--- src/attackmate/schemas/sleep.py
+++ src/attackmate/schemas/sleep.py
@@ -1,4 +1,7 @@
 from typing import Literal
+from pydantic import computed_field
+from functools import cached_property
+from random import randint
 from attackmate.schemas.base import BaseCommand, StringNumber
 from attackmate.command import CommandRegistry
 
@@ -10,3 +13,11 @@ class SleepCommand(BaseCommand):
     seconds: StringNumber = '1'
     random: bool = False
     cmd: str = 'sleep'
+
+    @computed_field  # type: ignore[misc]
+    @cached_property
+    # calculates the actual sleep time based on whether random is enabled
+    def sleep_time(self) -> int:
+        if not self.random:
+            return int(self.seconds)
+        return randint(int(self.min_sec), int(self.seconds))
