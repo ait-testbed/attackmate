5f6632c1da82b550b48705a0382ed00c97446feb
thorinaboenke <thorina-boenke@web.de>
2026-01-26 14:16:03 +0100

    async in Loop executor

--- src/attackmate/executors/common/loopexecutor.py
+++ src/attackmate/executors/common/loopexecutor.py
@@ -5,7 +5,7 @@ Execute commands in a loop
 """
 
 import copy
-from typing import Callable
+from typing import Callable, Awaitable
 from string import Template
 from attackmate.executors.baseexecutor import BaseExecutor
 from attackmate.executors.features.conditional import Conditional
@@ -27,7 +27,7 @@ class LoopExecutor(BaseExecutor):
         cmdconfig=None,
         *,
         varstore: VariableStore,
-        runfunc: Callable[[Commands], None],
+        runfunc: Callable[[Commands], Awaitable[None]],
         substitute_cmd_vars=False,
     ):
         self.runfunc = runfunc
@@ -58,7 +58,7 @@ class LoopExecutor(BaseExecutor):
                 new_val = Template(attr_val).safe_substitute(placeholders)
                 setattr(command_obj, attr_name, new_val)
 
-    def loop_range(self, command: LoopCommand, start: int, end: int) -> None:
+    async def loop_range(self, command: LoopCommand, start: int, end: int) -> None:
         for x in range(start, end):
             for cmd in command.commands:
                 template_cmd: Command = copy.deepcopy(cmd)
@@ -70,9 +70,9 @@ class LoopExecutor(BaseExecutor):
                 if self.break_condition_met(command, placeholders):
                     return
                 self.substitute_variables_in_command(template_cmd, placeholders)
-                self.runfunc([template_cmd])
+                await self.runfunc([template_cmd])
 
-    def loop_items(self, command: LoopCommand, varname: str, iterable: list[str]) -> None:
+    async def loop_items(self, command: LoopCommand, varname: str, iterable: list[str]) -> None:
         for x in iterable:
             for cmd in command.commands:
                 template_cmd: Command = copy.deepcopy(cmd)
@@ -84,9 +84,9 @@ class LoopExecutor(BaseExecutor):
                 if self.break_condition_met(command, placeholders):
                     return
                 self.substitute_variables_in_command(template_cmd, placeholders)
-                self.runfunc([template_cmd])
+                await self.runfunc([template_cmd])
 
-    def loop_until(self, command: LoopCommand, condition: str) -> None:
+    async def loop_until(self, command: LoopCommand, condition: str) -> None:
         x = 0
         tpl = Template(condition)
 
@@ -98,18 +98,18 @@ class LoopExecutor(BaseExecutor):
                     temp = Template(template_cmd.cmd)
                     template_cmd.cmd = temp.safe_substitute(LOOP_INDEX=x, **self.varstore.variables)
 
-                    self.runfunc([template_cmd])
+                    await self.runfunc([template_cmd])
                 else:
                     return
             x += 1
 
-    def execute_loop(self, command: LoopCommand) -> None:
+    async def execute_loop(self, command: LoopCommand) -> None:
         range_match = re.search(r'range\(\s*(\d+)\s*,\s*(\d+)\s*\)', command.cmd)
         if range_match:
             range_start, range_end = map(int, range_match.groups())
             if range_start > range_end:
                 raise ExecException('range_start is bigger than range_end')
-            self.loop_range(command, range_start, range_end)
+            await self.loop_range(command, range_start, range_end)
             return
 
         items_match = re.search(r'items\(\s*([^\)]+)\s*\)', command.cmd)
@@ -118,14 +118,14 @@ class LoopExecutor(BaseExecutor):
             listvar = self.varstore.get_list(var_name)
             if listvar is None:
                 raise ExecException(f"List variable '{var_name}' does not exist")
-            self.loop_items(command, var_name, listvar)
+            await self.loop_items(command, var_name, listvar)
             return
 
         until_match = re.search(r'until\((.*)\)', command.cmd)
         if until_match:
             condition = until_match.group(1)
             self.logger.info('Loop until ' + condition)
-            self.loop_until(command, condition)
+            await self.loop_until(command, condition)
             return
 
         self.logger.warning('No valid loop condition found in command: %s', command.cmd)
@@ -134,6 +134,6 @@ class LoopExecutor(BaseExecutor):
         # idea: use runfunc with one command only
         # in that way it is possible to replace context-variables first
         # runfunc will replace global variables then
-        self.execute_loop(command)
+        await self.execute_loop(command)
         self.logger.info('Loop execution complete')
         return Result('', 0)
